ff33ac067b7cc78fdc51d8acd81d11da971132cd
Aimé Labbé <aime.labbe@gmail.com>
2021-12-16 20:00:18 +0100

    add import_sdf2_file to module

--- vlf_mri/__init__.py
+++ vlf_mri/__init__.py
@@ -4,7 +4,7 @@ __author__ = "Aimé Labbé and Marie Poirier-Quinot"
 __credit__ = "Université Paris-Saclay, CEA, CNRS, Inserm, BioMaps"
 __all__ = ["import_vlf_file", "import_sdf_file", "FidData", "MagData", "RelData"]
 
-from vlf_mri.code.data_reader import import_sdf_file, import_vlf_file
+from vlf_mri.code.data_reader import import_sdf_file, import_sdf2_file, import_vlf_file
 
 from vlf_mri.code.fid_data import FidData
 from vlf_mri.code.mag_data import MagData
